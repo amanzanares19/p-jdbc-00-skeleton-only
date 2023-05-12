9d5c885b54acde617aac43197e93909a9d2b7639
amanzanares19 <amanzanares19@ilg.cat>
2023-05-12 18:35:10 +0200

    Video finalitzat

--- jdbc-demo-skeleton/src/com.pluralcamp.daw.persistence/com/pluralcamp/daw/persistence/daos/impl/jdbc/ColorDAOJDBCImpl.java
+++ jdbc-demo-skeleton/src/com.pluralcamp.daw.persistence/com/pluralcamp/daw/persistence/daos/impl/jdbc/ColorDAOJDBCImpl.java
@@ -4,10 +4,11 @@ import com.pluralcamp.daw.entities.core.Color;
 import com.pluralcamp.daw.persistence.daos.contracts.ColorDAO;
 import com.pluralcamp.daw.persistence.exceptions.DAOException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.sql.DriverManager;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.CallableStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -17,34 +18,33 @@ public class ColorDAOJDBCImpl implements ColorDAO {
 
         // Objectes que calen:
         // 1er objecte - Connexio, via DriverManager de JDBC
-        Connection conn = null;
         // 2n objecte - Obrir un canal de Consulta - PraparedStatement
-        PreparedStatement sentSQL = null;
         // 2.1 - Enviar la consulta SQL
-        ResultSet reader = null;
         // 3er objecte - Obrir un canal de Lectura, un cursor - ResultSet
         Color c = null;
 
-        try {
-            // Crear connexion
-            conn = DriverManager.getConnection("jdbc-mysql-//localhost:3306/calendar-restore?serverTimezone=Europe/Paris", "alex", "12345678");
-            
-            // Crear consulta
-            sentSQL = conn.prepareStatement("SELECT id, name, red, green, blue, FROM colors where id = ?");
+        try (// Crear connexion
+                Connection conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/calendar-restore?serverTimezone=Europe/Paris", "alex", "12345678");
+                // Crear consulta
+                CallableStatement sentSQL = conn
+                        .prepareCall("SELECT id, name, red, green, blue FROM colors where id = ?");) {
+
             sentSQL.setLong(1, id);
 
             // Leer consulta
-            reader = sentSQL.executeQuery();
-
-            if (reader.next()) {
-                c = new Color(reader.getString("name"), reader.getInt("red"), reader.getInt("green"), reader.getInt("blue"));
-                c.setId(reader.getLong("id"));
+            try (ResultSet reader = sentSQL.executeQuery();) {
+                if (reader.next()) {
+                    c = new Color(reader.getString("name"), reader.getInt("red"), reader.getInt("green"),
+                            reader.getInt("blue"));
+                    c.setId(reader.getLong("id"));
+                }
             }
 
-
         } catch (SQLException ex) {
-            System.out.println();
-        } finally {
+
+            // Logger
+            throw new DAOException(ex);
 
         }
 
@@ -53,7 +53,33 @@ public class ColorDAOJDBCImpl implements ColorDAO {
 
     @Override
     public List<Color> getColors() throws DAOException {
-        return null;
+
+        List<Color> colors = new ArrayList<Color>();
+
+        try (   // Crear connexion
+                Connection conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/calendar-restore?serverTimezone=Europe/Paris", "alex", "12345678");
+                // Crear consulta
+                CallableStatement sentSQL = conn.prepareCall("SELECT id, name, red, green, blue FROM colors");
+                // Leer consulta
+                ResultSet reader = sentSQL.executeQuery();) {
+
+            while (reader.next()) {
+                var c = new Color(reader.getString("name"), reader.getInt("red"), reader.getInt("green"),
+                        reader.getInt("blue"));
+                c.setId(reader.getLong("id"));
+                colors.add(c);
+            }
+
+        } catch (SQLException ex) {
+
+            // Logger
+            throw new DAOException(ex);
+
+        }
+
+        return colors;
+
     }
 
     @Override
--- jdbc-demo-skeleton/src/com.pluralcamp.daw.persistence/com/pluralcamp/daw/persistence/exceptions/DAOException.java
+++ jdbc-demo-skeleton/src/com.pluralcamp.daw.persistence/com/pluralcamp/daw/persistence/exceptions/DAOException.java
@@ -1,4 +1,21 @@
 package com.pluralcamp.daw.persistence.exceptions;
 
 public class DAOException extends Exception{
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DAOException(String message) {
+        super(message, null);
+    }
+    
+    public DAOException(Throwable cause) {
+        this(cause.toString(), cause);
+    }
+
+    public DAOException() {
+        this("Se ha producido un error en la capa de persistencia", null);
+    }
+
 }
